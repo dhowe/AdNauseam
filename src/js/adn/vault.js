214a98dabd6c389dcbec2d7abf1bca4dfdff8aa8
CyrusSUEN <yuyhero@gmail.com>
2016-06-12 15:41:09 +0800

    Fix #224

--- src/js/adn/vault.js
+++ src/js/adn/vault.js
@@ -1461,9 +1461,8 @@
 
       var filtered = dateFilter(gMin, gMax);
 
-      // only create the adsets once, else filter
-      return gAdSets ? filterAdSets(filtered) :
-        (gAdSets = createAdSets(filtered));
+      centerContainer();
+      return gAdSets = createAdSets(filtered);
     }
 
     function centerContainer() {
