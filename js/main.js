e2e9d2e1a10d22da2e04c823d3b4994ab6e99e4e
CyrusSUEN <yuyhero@gmail.com>
2016-07-19 22:50:22 +0800

    Fix #291

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	var original;
     $("#logo.subpage").mouseover(function(){
     	original= $(this).attr('src');
@@ -7,4 +8,21 @@ $(document).ready(function(){
     $("#logo.subpage").mouseout(function(){
         $(this).attr('src',original);
     });
+
+    const BIN_PATH = {
+        Firefox: "https://addons.mozilla.org/firefox/addon/adnauseam/",
+        Chrome: "https://chrome.google.com/webstore/detail/adnauseam/hgfacieeomogkcchookiodlpppbcolha",
+        Opera: "https://addons.opera.com/extensions/details/adnauseam-2/"
+    }
+
+    var binPath = BIN_PATH[bowser.name];
+    
+    // when it's not Firefox, Chrome or Opera
+    if (binPath == undefined) {
+        $("div#install").find("a").find("text").attr("x", "35");
+        $("div#install").find("a").find("text").text("Try it on Chrome, Firefox or Opera!");
+    }
+    else {
+        $("div#install").find("a").attr("xlink:href", binPath);
+    }
 });
